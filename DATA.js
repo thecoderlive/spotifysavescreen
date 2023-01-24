e8e0321f0af987c654a87580459a841bc4133239
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-01-24 12:54:59 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,42 @@
+export const item = {
+    "id": "0d2e5431-3cf2-46be-959a-ece84bdc49ef",
+    "bar_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/113.png",
+    "song_name": "contain",
+    "movie_name": "bag",
+    "clip_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/316.png",
+    "heart_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/225.png",
+    "plus_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/32.png",
+    "upload_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/35.png",
+    "song_list": [
+        {
+            "id": "433c447c-1bc9-4385-ac0d-639fcb299da8",
+            "serial_number": 4862,
+            "song_title": "Responsibility agree accept guy describe pull available. Doctor strategy PM.",
+            "about_song": "note"
+        },
+        {
+            "id": "339758a9-1dd8-4360-9ee3-52bc0b436e7c",
+            "serial_number": 871,
+            "song_title": "Sister expert example trade well win window relate. Impact build help.",
+            "about_song": "find"
+        },
+        {
+            "id": "937cfbca-6ae6-4c89-b1a7-7432bed10c34",
+            "serial_number": 6648,
+            "song_title": "Heavy community model federal. Maybe score appear stand state draw. Score late this difference film yourself.",
+            "about_song": "along"
+        },
+        {
+            "id": "166c616c-23ef-4970-94fb-47c158b21fa4",
+            "serial_number": 7032,
+            "song_title": "Her trial yes military. Itself own step assume national. Dream ask executive maintain forward sing produce last. Model production develop left number.",
+            "about_song": "cultural"
+        },
+        {
+            "id": "49b32f2d-569f-4aaa-a2f3-4b68dd2fe77b",
+            "serial_number": 4775,
+            "song_title": "Item focus property help. Consider vote machine him recent down.",
+            "about_song": "responsibility"
+        }
+    ]
+}
